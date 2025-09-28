fd0292695294aa1d253ea872456791e0d4f613d5
sajedur22 <151081528+sajedur22@users.noreply.github.com>
2025-09-28 12:02:06 +0600

    hero

--- components/common/hero.jsx
+++ components/common/hero.jsx
@@ -9,7 +9,7 @@ import LinkedinIcon from "@/public/images/linkedin.svg";
 import Link from "next/link";
 export function Hero() {
     return (
-        <section id={'home'} className=" h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden top-0 right-0 pt-4">
+        <section id={'home'} className=" h-[42rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden top-0 right-0 pt-3">
             <Spotlight />
 
             <div className={'flex flex-col md:flex-row justify-center items-center lg:px-[15%] md:gap-8 content-center px-16 gap-0'}>
@@ -21,9 +21,22 @@ export function Hero() {
                     height={512}
                     priority={true}
                 />
-                
+
                 <div className={'flex flex-col justify-center'}>
-                    <div className={'flex flex-row order-last md:order-first pt-5 md:pt-0 justify-center md:justify-start gap-4'}>
+
+
+                    <h1 className={'text-2xl md:text-6xl font-serif text-center md:text-start text-primary-foreground pt-4 md:pt-7 gap-2'}>
+                        Sajedur Rahman Shakil
+                    </h1>
+                    <p className={'font-serif text-muted-foreground text-sm md:text-xl sm:text-1xl text-center md:text-start pt-4 md:pt-8 pl-5 md:pl-2'}>
+                        I build scalable, production-ready web apps.
+                        Experienced in delivering SaaS,
+                        MVPs, and full-stack platforms with modern tech stacks.
+                        Open to remote roles & freelance projects.
+                    </p>
+
+                    <div className="flex flex-row md:order-first justify-center md:justify-start gap-4 mb-4 pt-2 md:mb-0">
+
                         <Link
                             href="https://github.com/sajedur22"
                             target="_blank"
@@ -53,12 +66,12 @@ export function Hero() {
                             />
                         </Link>
                         <Link
-                            href="sajedur645@gmail.com"
-                            target="_blank"
+                            href="mailto:sajedur645@gmail.com"
                             className="social-link"
                             aria-label="Send me an email"
                         >
-                            <Mail className="invert-0 dark:invert transition-all duration-300" width={24} height={24} />
+
+                            <Mail className="invert-0 dark:invert transition-all duration-300" width={24} height={24}/>
                         </Link>
                         <Link
                             href="/"
@@ -66,19 +79,10 @@ export function Hero() {
                             className="social-link"
                             aria-label="Download My Resume"
                         >
-                            <FileDown className="invert-0 dark:invert transition-all duration-300" width={24} height={24} />
+                            <FileDown className="invert-0 dark:invert transition-all duration-300" width={24}
+                                      height={24}/>
                         </Link>
                     </div>
-
-                        <h1 className={'text-2xl md:text-6xl font-serif text-center md:text-start text-primary-foreground pt-4 md:pt-7 gap-2'}>
-                            Sajedur Rahman <span className={'block lg:inline ter-primary'}>Shakil</span>
-                        </h1>
-                        <p className={'font-serif text-muted-foreground text-sm md:text-xl sm:text-1xl text-center md:text-start pt-4 md:pt-8 pl-5 md:pl-2'}>
-                            I build scalable, production-ready web apps.
-                            Experienced in delivering SaaS,
-                            MVPs, and full-stack platforms with modern tech stacks.
-                            Open to remote roles & freelance projects.
-                        </p>
 
                 </div>
             </div>
